30eb5b7c96bf6d062902d19ceef7204db6eac244
Heitor Pires <heitorfernandes_3@hotmail.com>
2021-10-31 17:16:22 -0300

    create styles of MessageList and Message components

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,15 +1,32 @@
 import React from 'react';
 
-import { View } from 'react-native';
+import { ScrollView } from 'react-native';
 
 import { Message } from '../Message';
 
 import { styles } from './styles';
 
 export function MessageList() {
+
+  const message = {
+    id: '1',
+    text: 'teste',
+    user: {
+      name: 'Heitor F. Pires',
+      avatar_url: 'https://github.com/heitor3.png',
+    }
+  }
+
   return (
-    <View style={styles.container}>
-      <Message />
-    </View>
+    <ScrollView
+      style={styles.container}
+      contentContainerStyle={styles.content}
+      keyboardShouldPersistTaps="never"
+    >
+      <Message data={message} />
+      <Message data={message} />
+      <Message data={message} />
+      <Message data={message} />
+    </ScrollView>
   );
 }
