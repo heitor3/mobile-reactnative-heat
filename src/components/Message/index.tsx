1a145218fc37d22f97a6a6d4af5307a4107b3a1a
Heitor Pires <heitorfernandes_3@hotmail.com>
2021-11-01 11:28:20 -0300

    animated with moti

--- src/components/Message/index.tsx
+++ src/components/Message/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-
 import { View, Text } from 'react-native';
+import { MotiView } from 'moti';
 import { UserPhoto } from '../UserPhoto';
 
 import { styles } from './styles';
@@ -20,7 +20,12 @@ type Props = {
 
 export function Message({ data }: Props) {
   return (
-    <View style={styles.container}>
+    <MotiView
+      from={{ opacity: 0, translateY: -50 }}
+      animate={{ opacity: 1, translateY: 0 }}
+      transition={{ type: 'timing', duration: 700 }}
+      style={styles.container}
+    >
       <Text style={styles.message}>
         {data.text}
       </Text>
@@ -36,6 +41,6 @@ export function Message({ data }: Props) {
         </Text>
       </View>
 
-    </View>
+    </MotiView>
   );
 }
